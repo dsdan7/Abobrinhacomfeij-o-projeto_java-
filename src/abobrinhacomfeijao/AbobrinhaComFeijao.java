805898aee650ececa683c3f6330172d9b6ae0ca8
dsdan7 <dsdsdan7@gmail.com>
2018-08-30 15:11:54 -0300

    Commit 2º

--- src/abobrinhacomfeijao/AbobrinhaComFeijao.java
+++ src/abobrinhacomfeijao/AbobrinhaComFeijao.java
@@ -9,7 +9,7 @@ public class AbobrinhaComFeijao {
     public static void main(String[] args) {
      int x=0;
      while(x==0){
-         System.out.println("OPA");
+         
          JOptionPane.showMessageDialog(null,"opa");
      }
         
