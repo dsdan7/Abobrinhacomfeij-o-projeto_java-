25e44f18c774e2473bc6d0c7c6a4777cb309773f
dsdan7 <dsdsdan7@gmail.com>
2018-08-30 14:45:08 -0300

    Commit 1º

---
+++ src/abobrinhacomfeijao/AbobrinhaComFeijao.java
@@ -0,0 +1,18 @@
+
+package abobrinhacomfeijao;
+
+import java.awt.Image;
+import javax.swing.JOptionPane;
+
+public class AbobrinhaComFeijao {
+
+    public static void main(String[] args) {
+     int x=0;
+     while(x==0){
+         System.out.println("OPA");
+         JOptionPane.showMessageDialog(null,"opa");
+     }
+        
+    }
+    
+}
